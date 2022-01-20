c88cd2b8788eba338d788062add4684750a66603
kevinnammour <kevin.nammour@lau.edu>
2022-01-20 22:24:00 +0200

    13th commit

--- public/js/dataset.js
+++ public/js/dataset.js
@@ -1,3 +1,4 @@
+// Slick, a jQuery plugin, is used for the slideshow
 $(".one-time").slick({
   dots: false,
   infinite: true,
@@ -12,35 +13,42 @@ $(".one-time").slick({
   responsive: "unslick",
 });
 
-copyCitation = () => {
+document.querySelector(".download__btn").onclick = () => {
+  // Downloads the file whose path is in the anchor #zip.
+  document.querySelector("#zip").click();
+};
+
+openCitation = () => {
+  // This is the copy icon of Citation. When clicked,
+  // it copies the text to clipboard, and closes the
+  // bibtex__modal if opened, and opens the citation__modal.
   navigator.clipboard.writeText(
     `Ismail, A.. (2022). HICMA, Lebanese American University.`
   );
-
-  conf = document.getElementById("copied");
-  conf.style.opacity = "1";
-  setTimeout(function () {
-    conf.style.opacity = "0";
-  }, 2 * 1000);
+  document.querySelector(".bibtex__modal").style.display = "none";
+  document.querySelector(".citation__modal").style.display = "table";
 };
 
-copyBibTex = () => {
+closeCitation = () => {
+  // It closes the citation__modal
+  document.querySelector(".citation__modal").style.display = "none";
+};
+
+openBibtex = () => {
+  // This is the copy icon of Bibtex. When clicked,
+  // it copies the text to clipboard, and closes the
+  // citation__modal if opened, and opens the bibtex__modal.
   navigator.clipboard.writeText(`@misc{hicma_dataset,
-        author={Ismail, A.},
-        title={{HICMA}},
-        year={2022},
-        howpublished={Lebanese American University}
-      }`);
-
-  conf = document.getElementById("copied");
-  conf.style.opacity = "1";
-  setTimeout(function () {
-    conf.style.opacity = "0";
-  }, 2 * 1000);
+    author={Ismail, A.},
+    title={{HICMA}},
+    year={2022},
+    howpublished={Lebanese American University}
+  }`);
+  document.querySelector(".citation__modal").style.display = "none";
+  document.querySelector(".bibtex__modal").style.display = "table";
 };
 
-download_btn = document.querySelector(".download__btn");
-
-download_btn.onclick = () => {
-    document.querySelector("#zip").click();
-}
+closeBibtex = () => {
+  // It closes the bibtex__modal
+  document.querySelector(".bibtex__modal").style.display = "none";
+};
--- public/js/goal.js
+++ public/js/goal.js
@@ -16,32 +16,81 @@ class Action {
   }
 }
 
-let action_1 = new Action('#title', undefined, -140, undefined, [1, 0]);
-let action_2 = new Action('.why-section-child', undefined, '-25%', undefined, [0, 1.75]);
-let action_3 = new Action('#arabic-letters', undefined, '-70%', undefined, [0, 1]);
-let action_4 = new Action('#letter-1', '-10%', '-15%', [1, 2], [1, 0]);
-let action_5 = new Action('#letter-2', '-4%', '-5%', undefined, [1, 0]);
-let action_6 = new Action('#letter-3', '8%', '-17%', [1, 1.5], [1, 0]);
-let action_7 = new Action('#letter-4', '-15%', '-2%', [1, 2], [1, 0]);
-let action_8 = new Action('#letter-5', '-7%', '-1%', [1, 1.2], [1, 0]);
-let action_9 = new Action('#letter-6', '12%', '-3%', [1, 1.8], [1, 0]);
-let action_10 = new Action('#letter-7', '5%', '14%', [1, 1.7], [1, 0]);
-let action_11 = new Action('#letter-8', '9%', '6%', [1, 2], [1, 0]);
-let action_12 = new Action('.why-section-child', undefined, ['-25%', '-40%'], undefined, [1, 0]);
-let action_13 = new Action('#reqs-section-child', undefined, '-25%', undefined, [0, 1.75]);
-let action_14 = new Action('#reqs-section-child', undefined, ['-25%', '-25%'], [1, 0.6], [1.75, -0.75]);
-let action_15 = new Action('#history', undefined, undefined, [0.8, 1], [0, 2]);
+let action_1 = new Action("#title", undefined, -140, undefined, [1, 0]);
+let action_2 = new Action(
+  ".why-section-child",
+  undefined,
+  "-25%",
+  undefined,
+  [0, 1.75]
+);
+let action_3 = new Action(
+  "#arabic-letters",
+  undefined,
+  "-70%",
+  undefined,
+  [0, 1]
+);
+let action_4 = new Action("#letter-1", "-10%", "-15%", [1, 2], [1, 0]);
+let action_5 = new Action("#letter-2", "-4%", "-5%", undefined, [1, 0]);
+let action_6 = new Action("#letter-3", "8%", "-17%", [1, 1.5], [1, 0]);
+let action_7 = new Action("#letter-4", "-15%", "-2%", [1, 2], [1, 0]);
+let action_8 = new Action("#letter-5", "-7%", "-1%", [1, 1.2], [1, 0]);
+let action_9 = new Action("#letter-6", "12%", "-3%", [1, 1.8], [1, 0]);
+let action_10 = new Action("#letter-7", "5%", "14%", [1, 1.7], [1, 0]);
+let action_11 = new Action("#letter-8", "9%", "6%", [1, 2], [1, 0]);
+let action_12 = new Action(
+  ".why-section-child",
+  undefined,
+  ["-25%", "-40%"],
+  undefined,
+  [1, 0]
+);
+let action_13 = new Action(
+  "#reqs-section-child",
+  undefined,
+  "-25%",
+  undefined,
+  [0, 1.75]
+);
+let action_14 = new Action(
+  "#reqs-section-child",
+  undefined,
+  ["-25%", "-25%"],
+  [1, 0.6],
+  [1.75, -0.75]
+);
+let action_15 = new Action("#history", undefined, undefined, [0.8, 1], [0, 2]);
 
-let scroll_1 = new Scroll('#intro', '100%', [action_1]);
-let scroll_2 = new Scroll('#why-section', '150%', [action_2, action_3]);
-let scroll_3 = new Scroll('#why-section', '150%', [action_4, action_5, action_6, action_7, action_8, action_9, action_10, action_11]);
-let scroll_4 = new Scroll('#why-section', '100%', [action_12]);
-let scroll_5 = new Scroll('#reqs-section', '150%', [action_13]);
-let scroll_6 = new Scroll('#reqs-section', '20%', []);
-let scroll_7 = new Scroll('#reqs-section', '100%', [action_14]);
-let scroll_8 = new Scroll('#history', '100%', [action_15]);
-let scroll_9 = new Scroll(undefined, '100%', []);
-scrolls = [scroll_1, scroll_2, scroll_3, scroll_4, scroll_5, scroll_6, scroll_7, scroll_8, scroll_9];
+let scroll_1 = new Scroll("#intro", "100%", [action_1]);
+let scroll_2 = new Scroll("#why-section", "150%", [action_2, action_3]);
+let scroll_3 = new Scroll("#why-section", "150%", [
+  action_4,
+  action_5,
+  action_6,
+  action_7,
+  action_8,
+  action_9,
+  action_10,
+  action_11,
+]);
+let scroll_4 = new Scroll("#why-section", "100%", [action_12]);
+let scroll_5 = new Scroll("#reqs-section", "150%", [action_13]);
+let scroll_6 = new Scroll("#reqs-section", "20%", []);
+let scroll_7 = new Scroll("#reqs-section", "100%", [action_14]);
+let scroll_8 = new Scroll("#history", "100%", [action_15]);
+let scroll_9 = new Scroll(undefined, "100%", []);
+scrolls = [
+  scroll_1,
+  scroll_2,
+  scroll_3,
+  scroll_4,
+  scroll_5,
+  scroll_6,
+  scroll_7,
+  scroll_8,
+  scroll_9,
+];
 
 const onLoad = () => {
   $(() => {
@@ -135,14 +184,6 @@ const onLoad = () => {
       $(window).scroll(0);
       curr_section = sections[0];
       $(curr_section).show();
-      $(window).resize(() => {
-        $("body").addClass("page-error");
-      });
-      if ("ontouchstart" in window || "onmsgesturechange" in window) {
-        $(window).resize(() => {
-          $("body").addClass("page-error");
-        });
-      }
     };
 
     reform = () => {
