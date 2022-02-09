56c8e83e538378b9a190ad643cc82901e0746f26
kevinnammour <nammourkevin@gmail.com>
2022-02-09 12:49:30 +0200

    nav_css clean

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,25 +1,13 @@
 const navMenu = document.getElementById("nav-menu"),
-  toggleMenu = document.getElementById("open-nav"),
+  openMenu = document.getElementById("open-nav"),
   closeMenu = document.getElementById("close-nav");
 
-toggleMenu.addEventListener("click", () => {
+openMenu.addEventListener("click", () => {
   navMenu.classList.toggle("show");
 });
 
 closeMenu.addEventListener("click", () => {
   navMenu.classList.remove("show");
-});
-
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
-
-const optionsList = document.querySelectorAll(".option");
-
-optionsList.forEach((o) => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
-  });
 });
 
 var loader = document.getElementById("preloader");
