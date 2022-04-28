b471725e95bbf93887a98e276a79256ef245c724
kevinnammour <nammourkevin@gmail.com>
2022-04-28 18:59:09 +0300

    contact form

--- public/js/contactUs.js
+++ public/js/contactUs.js
@@ -1,42 +1,6 @@
-copy = (id) => {
-  navigator.clipboard.writeText(document.getElementById(id).innerHTML);
-  showConfirmation();
-}
-
-showConfirmation = () => {
-  var copied = document.createElement("div");
-  copied.style.opacity = 0;
-  copied.style.position = "fixed";
-  copied.style.display = "flex";
-  copied.style.alignItems = "center";
-  copied.style.bottom = "25px";
-  copied.style.right = "25px";
-  copied.style.width = "240px";
-  copied.style.height = "55px";
-  copied.style.boxSizing = "border-box";
-  copied.style.padding = "15px";
-  copied.style.backgroundColor = "#cbbd78";
-  copied.style.color = "white";
-  copied.style.borderRadius = "8px";
-  copied.style.boxShadow = "rgba(149, 157, 165, 0.15) 0px 8px 24px";
-  copied.setAttribute("class", "copied");
-
-  var clipboardIcon = document.createElement("ion-icon");
-  clipboardIcon.setAttribute("name", "clipboard");
-  clipboardIcon.style.fontSize = "1.3rem";
-
-  var span = document.createElement("span");
-  span.style.marginLeft = "15px";
-  span.innerText = "Copied to clipboard!";
-
-  $(copied).append(clipboardIcon);
-  $(copied).append(span);
-  $("body").append(copied);
-
-  copied.style.opacity = "1";
-  setTimeout(function () {
-    copied.style.opacity = "0";
-  }, 2000);
+submitForm = () => {
+  const form = document.getElementById("form");
+  form.submit();
 };
 
 // The below code is to allow scaling when users hover on the image
@@ -63,12 +27,4 @@ map.on("mousemove", function (e) {
 
 map.on("mouseout", function () {
   $(this).children("#map-link").css({ transform: "scale(1)" });
-});
-
-$("#fixed__contact__btn").click(function () {
-  $("#fixed__contact__form").css("transform", "translateX(0px)");
-});
-
-$("#close__contact__form").click(function () {
-  $("#fixed__contact__form").css("transform", "translateX(-150%)");
 });
