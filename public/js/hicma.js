e419986036baa8c87b83f44fe189f713a4d37599
kevinnammour <nammourkevin@gmail.com>
2022-05-19 11:04:12 +0300

    dataset + minify js files

--- public/js/hicma.js
+++ public/js/hicma.js
@@ -1,14 +1,1 @@
-const loader__line = document.querySelector(".loader__line");
-const main = document.querySelector(".main-content");
-
-function init() {
-  setTimeout(() => {
-    loader__line.style.opacity = 0;
-    loader__line.style.display = "none";
-
-    main.style.display = "block"; 
-    setTimeout(() => (main.style.opacity = 1), 50);
-  }, 3000);
-}
-
-init();
+const loader__line=document.querySelector(".loader__line"),main=document.querySelector(".main-content");function init(){setTimeout(()=>{loader__line.style.opacity=0,loader__line.style.display="none",main.style.display="block",setTimeout(()=>main.style.opacity=1,50)},3e3)}init();
